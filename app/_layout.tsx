9c5f00ce51b7191b23aa2b4148511d5cd91256c4
Cauany Rodrigues <cauanyrodrigues01@gmail.com>
2024-11-18 21:02:51 -0300

    tratado erros devido o merge

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Stack } from 'expo-router/stack';
 import { StatusBar } from "expo-status-bar";
 
@@ -10,44 +9,3 @@ export default function Layout() {
     </Stack>
   );
 }
-=======
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
->>>>>>> 9f1c7c20de232ec5617e809acfb66395c1dcef0f
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
@@ -15,31 +14,6 @@ export default {
     background: '#000',
     tint: tintColorDark,
     tabIconDefault: '#ccc',
-=======
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
-export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
->>>>>>> 9f1c7c20de232ec5617e809acfb66395c1dcef0f
     tabIconSelected: tintColorDark,
   },
 };
